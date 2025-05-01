e20d6e1d72c6dd8badfdc6a27ca641180d741ff5
Sam <sam.brown@madetech.com>
2025-05-01 15:47:12 +0100

    add test env and security group to database

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,8 +22,25 @@ resource "aws_db_subnet_group" "mysite" {
   }
 }
 
+resource "aws_security_group" "postgres_sg" {
+  ingress = {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
 resource "aws_db_instance" "mysite" {
-  allocated_storage    				= 10
+  allocated_storage    				= 2
   db_name              				= "mydb"
 	db_subnet_group_name 				= aws_db_subnet_group.mysite.name
   engine              			  = "postgres"
@@ -32,4 +49,30 @@ resource "aws_db_instance" "mysite" {
   username             				= "foo"
 	manage_master_user_password = true
   skip_final_snapshot  				= true
+  vpc_security_group_ids = [ aws_security_group.postgres_sg.id ]
 }
+
+# resource "aws_security_group" "redis_sg" {
+#   name        = "redis-security-group"
+#   description = "Security group for Redis cluster"
+
+#   ingress {
+#     from_port   = 6379
+#     to_port     = 6379
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  
+#   }
+# }
+
+# resource "aws_elasticache_cluster" "mysite-redis" {
+#   cluster_id           = "mysite"
+#   engine               = "redis"
+#   node_type            = "cache.t3.micro"  
+#   num_cache_nodes      = 1
+#   parameter_group_name = "default.redis7"  
+#   engine_version       = "7.0"            
+#   apply_immediately    = true
+#   port                 = 6379
+
+#   security_group_ids   = [aws_security_group.redis_sg.id] 
+# }
